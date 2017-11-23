e849565d44e2e37567586778d1d405ac454f8202
Dagrond <ZiomuuSs@gmail.com>
2017-11-23 16:52:14 +0100

    init

---
+++ src/com/gmail/ZiomuuSs/IncomeBuildings.java
@@ -0,0 +1,12 @@
+package com.gmail.ZiomuuSs;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class IncomeBuildings extends JavaPlugin {
+  
+  public void onEnable() {
+    Bukkit.getLogger().info("Plugin wlaczony! Yay!");
+  }
+  
+}
